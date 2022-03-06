54c9b8d4c78b29b17efe638fc1f87b4b403bb447
Firas Abilmona <firas.abilmona@gmail.com>
2022-03-05 23:30:32 -0800

    Init Commit

---
+++ js/script.js
@@ -0,0 +1,116 @@
+// Assignment One
+    // 2. numeric litela expression
+    // 5.5;
+
+    // String literal expression
+    // 'Hi there';
+
+    // Boolean
+    // false;
+
+    // Null literal
+    // null;
+
+    // 3. Complex expression
+    // let anExpression = 4 * (3 + 5) / 2;
+
+    // variable expression
+    // let num = prompt ('Give me a number btween 1 to 9');
+    
+    // 4. Declare but do not assign
+    // let firstName; let lastName; let streetAdress; let cityName; let stateName; let zipCode; let yourAge;
+    // let referralSource; let mayWeContactYou;
+
+    // 5. Declare and assign 3 different ways
+    // myName = 'Firas';
+    // lastName = 'Abilmona';
+    // let streetAdress = '123 4th street'; let cityName = 'San Diego';
+    // let zipCode = '92123', yourAge = '43';
+
+    // 6. Create a variable
+    //  let num = 5
+    //  let string = 5
+    //  console.log(num + string)
+    //    
+    // 7. Create 2 variables
+    // Boolean value = false
+    // let string = 100
+    // console.log(boolean + string)
+    // console.log(string + boolean)
+    // 
+    // 8. Valid String
+    // let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+    // window.console.log(someString);
+    //
+    // 9. Create varaiables that produces null and unidentified
+    // var name;
+    // console.log( name );
+    // var c;
+    // console.log( c )
+    //
+    // 10. typeof
+    // console.log(typeof 'Hello');
+    // output: "string"
+    // console.log(typeof 42);
+    // output: "number"
+    // console.log(typeof true);
+    // output: "boolean"
+    // console.log(typeof null)
+    // output: "Object"
+    // console.log(typeof Variable);
+    // output: "undefined"
+    //
+    // 11. Alerts
+    // let firstGreeting = 'Hello '
+    // let secondGreeting = ', welcome to the JavaScript class!'
+    // alert(firstGreeting + 'Firas Abilmona' + secondGreeting)
+    //
+    // 12. 
+    // var name = 'Firas Abilmona'
+    // let firstGreeting = 'Hello '
+    // let secondGreeting = ', welcome to the JavaScript class!'
+    // alert(firstGreeting + name + secondGreeting)
+    //
+    // 13. 
+    // let firstGreeting = 'Hello '
+    // var name = 'Firas Abilmona'
+    // let secondGreeting = ', welcome to the '
+    // var course = 'JavaScript'
+    // alert(firstGreeting + name + secondGreeting + course + ' class!')
+    //
+    // 14.
+    // alert('Hello Firas Abilmona \n Welcome to the JavaScript Class!')
+    //
+    // 15.
+    // var name = prompt('Enter your name')
+    // console.log(alert('Hello ' + name + ' Weclome to the JavaScript class!'))
+    //
+    // 16.
+    // var className = prompt('Enter the class name that you are attending')
+    // console.log(alert('Hello Firas Abilmona. Weclome to the ' + className))
+    //
+    // 17.
+    // var x = 10
+    // var y = 20
+    // console.log(x + y)
+    //
+    // 18.
+    // var x = 20
+    // console.log(x + 20)
+    //
+    // 19.
+    // var x = 20
+    // console.log(x * 5)
+    //
+    // 20.
+    // let x = 20 =% 3
+    // console.log(x + 1)
+    //
+    // 21.
+    // let num = prompt('What is 1 + 1?')
+    // if (num = 2) {
+    //     console.log(true)
+    // }
+    //
+    // 22.
+    // console.log(confirm('The earth is flat!'))
